18adfda847ee103af1dd753ef07e3b11311bbb24
Hamas-Qureshi <hqure031@uottawa.ca>
2021-06-27 13:35:54 -0400

    ph

--- Lab6/main.js
+++ Lab6/main.js
@@ -45,9 +45,8 @@ $("#phone").keyup(function(){
 
     if (!((filter.test(numEntered) == true) || numEntered.length == 10)) {
         $(this).css("background-color", "pink");
-        alert("Please make sure to enter the correct amount of Digits");
         phoneVerified = false;
-        
+        alert("Please make sure to enter the correct amount of Digits");
     }
     else{
         $(this).css("background-color", "white");
